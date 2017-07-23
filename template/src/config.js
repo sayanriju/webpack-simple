66f371a397d14ef99ea0c397e3268ddd9968ec37
Sayan 'Riju' Chakrabarti <s26c.sayan@gmail.com>
2017-07-23 13:40:35 +0530

    Make vue-authenticate work with vue-axios

--- template/src/config.js
+++ template/src/config.js
@@ -1,4 +1,4 @@
 export default {
   apiRoot: "http://localhost:3000/api/v1",
-  tokenName: "603f0d3f9e5b489a864e867302a31389"  // a random uuid
+  tokenName: "token"  // in which the server sends the token
 }
--- template/src/main.js
+++ template/src/main.js
@@ -12,12 +12,36 @@ Vue.use(VueAxios)
 Vue.use(VueAuthenticate, {
   baseUrl: config.apiRoot, // Your API domain
   loginUrl: "/login",
-  tokenName: config.tokenName
+  tokenName: config.tokenName,
+
+  // Stuff to make it work with Axios (until vue-authenticate uses axios as official instead of vue-resource)
+  requestDataKey: "data",
+  responseDataKey: "data",
+  bindRequestInterceptor() {
+    this.$http.interceptors.request.use((axiosConfig) => {
+      if (this.isAuthenticated()) {
+        axiosConfig.headers["Authorization"] = [
+          this.options.tokenType, this.getToken()
+        ].join(" ")
+      } else {
+        delete axiosConfig.headers["Authorization"]
+      }
+      return axiosConfig
+    })
+  },
+
+  bindResponseInterceptor() {
+    this.$http.interceptors.response.use((response) => {
+      this.setToken(response)
+      return response
+    })
+  }
+
 })
 
 
 new Vue({   // eslint-disable-line
+  routes,
   el: "#app",
   render: h => h(App),
-  routes
 })
